5c076bf256bee8e69c104be45006a4b72ecd436f
Tulagunsmith <tulagunsmith@gmail.com>
2022-07-21 21:39:48 +0300

    directory status check added to create_dir method

--- yandex_api_class.py
+++ yandex_api_class.py
@@ -11,13 +11,19 @@ class YaUploader:
 
     def create_dir(self, dir_name):
         put_url = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {'path': dir_name, 'overwrite': 'true'}
-        response = requests.put(url=put_url, headers=self.headers, params=params)
-        return response.json()
+        params = {'path': dir_name}
+        response = requests.get(url=put_url, headers=self.headers, params=params)
+        if response.status_code == 404:
+            params = {'path': dir_name, 'overwrite': 'true'}
+            response = requests.put(url=put_url, headers=self.headers, params=params)
+            # return response.json()
+            print('Directory created.')
+        else:
+            print('Directory already exists.')
 
-    def upload_file(self):
+    def upload_file(self, dir_name, file_name):
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        upload_path = 'Netology-456/1'
+        upload_path = f'{dir_name}/{file_name}'
         file_url = 'https://sun1-88.userapi.com/impf/4ZoyoamCuqCTBMvJRACC34hZJ5gwmTMJVzcZfA/MkPV5C3t7hM.jpg?size=1620x2160&quality=96&sign=93bf73b835c03657fb27fae9748441ec&c_uniq_tag=4Wzv77FjOmjE34dx4uXLyc4VqVQPUsAi0G5q2CRuico&type=album'
         params = {
             'path': upload_path,
