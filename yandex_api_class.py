c81f5b6c0ae9f1686830eaafd1f95b1a8afe4344
Tulagunsmith <tulagunsmith@gmail.com>
2022-07-24 19:24:59 +0300

    file existance check added to uplosd_file method

--- yandex_api_class.py
+++ yandex_api_class.py
@@ -15,35 +15,34 @@ class YaUploader:
         response = requests.get(url=put_url, headers=self.headers, params=params)
         if response.status_code == 404:
             params = {'path': dir_name, 'overwrite': 'true'}
-            response = requests.put(url=put_url, headers=self.headers, params=params)
+            requests.put(url=put_url, headers=self.headers, params=params)
             # return response.json()
             print('Directory created.')
         else:
             print('Directory already exists.')
 
-    def upload_file(self, dir_name, file_name):
-        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        upload_path = f'{dir_name}/{file_name}'
-        file_url = 'https://sun1-88.userapi.com/impf/4ZoyoamCuqCTBMvJRACC34hZJ5gwmTMJVzcZfA/MkPV5C3t7hM.jpg?size=1620x2160&quality=96&sign=93bf73b835c03657fb27fae9748441ec&c_uniq_tag=4Wzv77FjOmjE34dx4uXLyc4VqVQPUsAi0G5q2CRuico&type=album'
-        params = {
-            'path': upload_path,
-            'url': file_url,
-            'overwrite': 'true'
-        }
-        response = requests.post(url=url, headers=self.headers, params=params)
-        return response.json()
-
-    # def get_upload_url(self, file_path):
-    #     params = {'path': file_path, 'overwrite': 'true'}  # file_path - путь, где файл будет в яндексе
-    #     response = requests.get(url=self.url, headers=self.headers, params=params)
-    #     return response.json()
-
-    # def upload(self, file_path: str):
-    #     file_name = os.path.basename(file_path)
-    #     href = self.get_upload_url(file_name).get("href", "")
-    #     response = requests.put(url=href, data=open(file_path, 'rb'))  # file_name - путь до файла в системе
-    #     response.raise_for_status()
-    #     if response.status_code == 201:
-    #         print("Success")
-    #     else:
-    #         print('God damn you!')
+    def upload_file(self, dir_name, file_name, file_url, file_date):
+        put_url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params = {'path': f'{dir_name}/{file_name}'}
+        response = requests.get(url=put_url, headers=self.headers, params=params)
+        if response.status_code == 404:
+            url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+            upload_path = f'{dir_name}/{file_name}'
+            # file_url = 'https://sun1-88.userapi.com/impf/4ZoyoamCuqCTBMvJRACC34hZJ5gwmTMJVzcZfA/MkPV5C3t7hM.jpg?size=1620x2160&quality=96&sign=93bf73b835c03657fb27fae9748441ec&c_uniq_tag=4Wzv77FjOmjE34dx4uXLyc4VqVQPUsAi0G5q2CRuico&type=album'
+            params = {
+                'path': upload_path,
+                'url': file_url,
+                'overwrite': 'true'
+            }
+            response = requests.post(url=url, headers=self.headers, params=params)
+            return response.json()
+        else:
+            url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+            upload_path = f'{dir_name}/{str(file_name) + str(file_date)}'
+            params = {
+                'path': upload_path,
+                'url': file_url,
+                'overwrite': 'true'
+            }
+            response = requests.post(url=url, headers=self.headers, params=params)
+            return response.json()
