6289802a9966e056e4cff8b7a0bfd09a7d84fcac
Tulagunsmith <tulagunsmith@gmail.com>
2022-07-24 19:26:11 +0300

    json elements renamed

--- vk_api.py
+++ vk_api.py
@@ -24,19 +24,18 @@ class Vk:
         for item in raw_list:
             sizes = item['sizes']
             likes = item['likes']['count']
-            photo_id = item['id']
             date = item['date']
             photo_info = {}
             for photo in sizes:
                 photo_info['size'] = 'a'
-                photo_info['likes'] = likes
+                photo_info['file_name'] = likes
                 photo_info['date'] = date
                 if photo['type'] == 'w':
-                    photo_info[photo_id] = photo['url']
+                    photo_info['photo_url'] = photo['url']
                     photo_info['size'] = photo['type']
                     break
                 elif ord(photo_info['size']) <= ord(photo['type']):
                     photo_info['size'] = photo['type']
-                    photo_info[photo_id] = photo['url']
+                    photo_info['photo_url'] = photo['url']
             photo_json.append(photo_info)
         return photo_json
