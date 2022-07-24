c3c630d4a2a0abf70570d5b3a0bacfe527fd1621
Tulagunsmith <tulagunsmith@gmail.com>
2022-07-25 00:25:20 +0300

    photo quantity function added, required inputs added

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from vk_api import Vk
 from pprint import pprint
+from tokens import vk_token
+from tokens import user_id
+from tokens import yandex_token
+from yandex_api_class import YaUploader
+
+
+def photos_upload_quantity(photos_list):
+    photos = input('How many photos would you like to upload?')
+    if not photos.isdigit() or int(photos) > len(photos_list):
+        return 5
+    else:
+        return int(photos)
+
 
 if __name__ == '__main__':
-    user_id = '289854633'
-    access_token = 'vk1.a.FzH7FIejX2D1IpjZg_aeijq2AidOJibdTNFbjqFkeIJUWLipr87v0zRJL0jOklCQtV_wVsXqoZF3BXbqGwVnTL8kJ1QqH_z4M2bSUqmRS9Mjp83ftDxQEYWWh5Z0d4LI-yBD7qA5MPC1T9ZTyJxFoPa2whkP4a0EGWvLJSU7xtz6dn4hJOlxoFANYSirkvC0'
-    vk = Vk(access_token, user_id)
-    pprint(vk.get_users_photos())
+    user_id_number = user_id
+    access_token = vk_token
+    album_owner_id = input('Input album owner id: ')
+    vk = Vk(access_token, user_id_number)
+    user_photo = vk.get_photo_url(album_owner_id)
+    photo_quantity = photos_upload_quantity(user_photo)
+    directory = input('Input the name of the directory: ')
+    yandex = YaUploader(token=yandex_token)
+    yandex.create_dir(dir_name=directory)
+    for item in user_photo[:photo_quantity]:
+        yandex.upload_file(dir_name=directory, file_name=item['file_name'], file_url=item['photo_url'], file_date=item['date'])
