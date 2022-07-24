f22e4cc0ae4ca72bcc42aa8db77bb37def88489d
Tulagunsmith <tulagunsmith@gmail.com>
2022-07-24 20:47:04 +0300

    upload method edited, log phrases added

--- yandex_api_class.py
+++ yandex_api_class.py
@@ -28,21 +28,22 @@ class YaUploader:
         if response.status_code == 404:
             url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
             upload_path = f'{dir_name}/{file_name}'
-            # file_url = 'https://sun1-88.userapi.com/impf/4ZoyoamCuqCTBMvJRACC34hZJ5gwmTMJVzcZfA/MkPV5C3t7hM.jpg?size=1620x2160&quality=96&sign=93bf73b835c03657fb27fae9748441ec&c_uniq_tag=4Wzv77FjOmjE34dx4uXLyc4VqVQPUsAi0G5q2CRuico&type=album'
             params = {
                 'path': upload_path,
                 'url': file_url,
                 'overwrite': 'true'
             }
             response = requests.post(url=url, headers=self.headers, params=params)
+            print('Copying photo!..')
             return response.json()
-        else:
+        elif response.status_code == 200:
             url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-            upload_path = f'{dir_name}/{str(file_name) + str(file_date)}'
+            upload_path = f'{dir_name}/{str(file_name) + " " + str(file_date)}'
             params = {
                 'path': upload_path,
                 'url': file_url,
                 'overwrite': 'true'
             }
             response = requests.post(url=url, headers=self.headers, params=params)
+            print('Photo with such name already exists. Renaming the photo...')
             return response.json()
